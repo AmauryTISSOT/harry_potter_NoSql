d287436c16e3476077d81d414e7068d2b207e742
SatyaMinguez <72600190+Satyaminguez@users.noreply.github.com>
2025-04-16 14:17:45 +0200

    routes CRUD characters

--- back/app/routes.py
+++ back/app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from . import mongo
 from .controllers.getcontroller import (get_character_by_id, get_nb_student_per_house,get_nb_characters_per_species,get_nb_wood_per_wand,get_nb_core_per_wand,get_nb_characters_in_movies, get_student_staff_or_nohogwarts, get_gender_in_hogwarts, get_gender_per_house, get_ancestry_in_hogwarts,get_ancestry_in_each_house, get_alive_or_dead)
+from .controllers.characterController import (update_character, create_character, delete_character)
 
 main = Blueprint('main', __name__)
 
@@ -59,3 +60,15 @@ def route_get_ancestry_in_each_house():
 @main.route('/aliveordead', methods=['GET'])
 def route_get_alive_or_dead():
     return get_alive_or_dead()
+
+@main.route('/characters/<character_id>', methods=['PUT'])
+def route_update_character(character_id):
+    return update_character(character_id)
+ 
+@main.route('/characters', methods=['POST'])
+def route_create_character():
+    return create_character()
+ 
+@main.route('/characters/<character_id>', methods=['DELETE'])
+def route_delete_chracter(character_id):
+    return delete_character(character_id)
