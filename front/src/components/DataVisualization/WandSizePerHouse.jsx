3507bd3fd47848dbf9ace37fe571bcb7098c7e3b
AmauryTISSOT <104317431+AmauryTISSOT@users.noreply.github.com>
2025-04-16 16:17:00 +0200

    feat: Create WandSizePerHouse graph

---
+++ front/src/components/DataVisualization/WandSizePerHouse.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import {
+    ResponsiveContainer,
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+} from "recharts";
+import CharactersApi from "../../services/CharactersApi";
+
+const WandSizePerHouse = () => {
+    const [rawData, setRawData] = useState([]);
+
+    const fetchData = async () => {
+        try {
+            const response = await CharactersApi.wandsizeperhouse();
+            setRawData(response.data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const data = rawData.map((item) => ({
+        gender: item._id.charAt(0).toUpperCase() + item._id.slice(1),
+        average: item.average,
+    }));
+
+    return (
+        <div
+            style={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+            }}
+        >
+            <h3>Taille moyenne des baguettes par maison</h3>
+            <ResponsiveContainer width="60%" height={450}>
+                <BarChart
+                    data={data}
+                    margin={{ top: 20, right: 30, left: 20, bottom: 20 }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="gender" />
+                    <YAxis domain={[0, 14]} />
+                    <Tooltip />
+                    <Legend />
+                    <Bar
+                        dataKey="average"
+                        fill="#8884d8"
+                        name="Taille moyenne des baguettes"
+                    />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
+    );
+};
+
+export default WandSizePerHouse;
