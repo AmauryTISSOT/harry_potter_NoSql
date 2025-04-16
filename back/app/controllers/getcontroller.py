ccea965e42522a047339bcae5d04216651219103
Juliette Mylle <154231973+JulietteMylle@users.noreply.github.com>
2025-04-16 10:54:48 +0200

    get principaux terminés

--- back/app/controllers/getcontroller.py
+++ back/app/controllers/getcontroller.py
@@ -102,5 +102,78 @@ def get_student_staff_or_nohogwarts() :
           return jsonify(results), 200
      else:
           return jsonify({'error': "calcul impossible"}), 404
+     
+def get_gender_in_hogwarts():
+     results = list(mongo.characters.aggregate([
+          {"$match" : {'$and' : [
+               {"hogwartsStudent": True},
+               {"house": {"$ne": ""}}
+               ]}},
+               {"$project": {
+                    "gender": {
+                         "$switch" : {
+                              "branches" : [
+                                   {"case" : {"$eq" : ["$gender", "male"]}, "then" : "male"},
+                                   {"case" : {"$eq" : ["$gender", "female"]}, "then" : "female"},
+                                   ],
+                                   "default": "non connu"
+                                   }
+                                   }
+                                   }
+                                   },
+                                   {"$group" : {"_id" : "$gender", "nb" : {"$sum" : 1 }}}
+                                   ]))
+     if results:
+          return jsonify(results), 200
+     else:
+          return jsonify({'error': "calcul impossible"}), 404
 
+
+def get_gender_per_house() :
+     results = list(mongo.characters.aggregate([
+          {"$match" : { "$and" : [
+               {"hogwartsStudent" : True},
+               {"house" : {"$ne" : ""}}
+          ]}},
+          {"$project" : {
+               "house" : 1,
+               "gender" : 1
+          }},
+          {"$group" : {"_id" : {"house" : "$house", "gender" : "$gender"}, "number" : {"$sum" : 1}}}
+
+     ]))
+     if results:
+          return jsonify(results), 200
+     else:
+          return jsonify({'error': "calcul impossible"}), 404
+
+def get_ancestry_in_hogwarts() : 
+     results = list(mongo.characters.aggregate([
+          {"$match" : { "$and" : [
+               {"hogwartsStudent" : True},
+               {"ancestry" : {"$ne" : ""}}
+          ]}},
+          {"$group" : {"_id" : "$ancestry", "number" : {"$sum" : 1}}}
+     ]))
+     if results:
+          return jsonify(results), 200
+     else:
+          return jsonify({'error': "calcul impossible"}), 404
+     
+def get_ancestry_in_each_house() : 
+     results = list(mongo.characters.aggregate([
+          {"$match" : { "$and" : [
+               {"hogwartsStudent" : True},
+               {"ancestry" : {"$ne" : ""}}
+          ]}},
+          {"$project" : {
+               "house" : 1,
+               "ancestry" : 1
+          }},
+          {"$group" : {"_id" : {"house" : "$house", "ancestry" : "$ancestry"}, "number" : {"$sum" : 1}}}
+     ]))
+     if results:
+          return jsonify(results), 200
+     else:
+          return jsonify({'error': "calcul impossible"}), 404
      
