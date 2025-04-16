d3a88181675d0463138a13b4116754f15c5496b7
AmauryTISSOT <104317431+AmauryTISSOT@users.noreply.github.com>
2025-04-16 11:35:59 +0200

    feat : add getAllCharacters API endpoint

--- front/src/services/CharactersApi.js
+++ front/src/services/CharactersApi.js
@@ -4,6 +4,9 @@ import axios from "axios";
 const API_BASE_URL = Apiconfig.API_BASE_URL;
 
 const CharactersApi = {
+    getAllCharacters: async () => {
+        return await axios.get(`${API_BASE_URL}/characters`);
+    },
     stundentperhouse: async () => {
         return await axios.get(`${API_BASE_URL}/studentperhouse`);
     },
