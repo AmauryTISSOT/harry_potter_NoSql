212fafce63f41a624259f4635e58019dc2f288a4
AmauryTISSOT <104317431+AmauryTISSOT@users.noreply.github.com>
2025-04-17 10:57:20 +0200

    feat: implement AddNewCharacterForm component with validation and styling

--- front/src/pages/AddNewCharacter/AddNewCharacter.jsx
+++ front/src/pages/AddNewCharacter/AddNewCharacter.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import AddNewCharacterForm from "../../components/AddNewCharacterForm/AddNewCharacterForm";
+import styles from "./AddNewCharacter.module.css";
 
 const AddNewCharacter = () => {
-    return <div>AddNewCharacter</div>;
+    return (
+        <div className={styles.formContainer}>
+            <AddNewCharacterForm />
+        </div>
+    );
 };
 
 export default AddNewCharacter;
