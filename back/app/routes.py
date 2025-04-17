e863b96988fef517a262f776b3042150e4885ed3
Juliette Mylle <154231973+JulietteMylle@users.noreply.github.com>
2025-04-17 12:24:02 +0200

    controllers films

--- back/app/routes.py
+++ back/app/routes.py
@@ -5,7 +5,7 @@ from . import mongo
 from .controllers.getcontroller import (get_character_by_id, get_nb_student_per_house,get_nb_characters_per_species,get_nb_wood_per_wand,get_nb_core_per_wand,get_nb_characters_in_movies, get_student_staff_or_nohogwarts, get_gender_in_hogwarts, get_gender_per_house, get_ancestry_in_hogwarts,get_ancestry_in_each_house, get_alive_or_dead)
 from .controllers.characterController import (update_character, create_character, delete_character)
 from .controllers.getcontroller import (get_character_by_id, get_nb_student_per_house,get_nb_characters_per_species,get_nb_wood_per_wand,get_nb_core_per_wand,get_nb_characters_in_movies, get_student_staff_or_nohogwarts, get_gender_in_hogwarts, get_gender_per_house, get_ancestry_in_hogwarts,get_ancestry_in_each_house, get_alive_or_dead, get_nb_of_patronus, get_wand_size, get_avg_wand_size_by_gender, get_avg_wand_size_per_house, get_muggle_vs_wizard, get_death_by_gender)
-from .controllers.moviescontroller import (get_apparition_total)
+from .controllers.moviescontroller import (get_apparition_total,get_apparaition_movie_one, get_apparaition_movie_two, get_apparaition_movie_three,get_apparaition_movie_four, get_apparaition_movie_five, get_apparaition_movie_six, get_apparaition_movie_seven, get_apparaition_movie_eight)
 
 main = Blueprint('main', __name__)
 
@@ -118,3 +118,38 @@ def route_delete_spell(spell_id):
 @main.route('/apparitionstotal', methods=['GET'])
 def route_get_apparition_total():
     return get_apparition_total()
+
+@main.route('/apparitionsmovieone', methods=['GET'])
+def route_get_apparaition_movie_one():
+    return get_apparaition_movie_one()
+
+@main.route('/apparitionsmovietwo', methods=['GET'])
+def route_get_apparaition_movie_two():
+    return get_apparaition_movie_two()
+
+@main.route('/apparitionsmoviethree', methods=['GET'])
+def route_get_apparaition_movie_three():
+    return get_apparaition_movie_three()
+
+
+
+@main.route('/apparitionsmoviefour', methods=['GET'])
+def route_get_apparaition_movie_four():
+    return get_apparaition_movie_four()
+
+@main.route('/apparitionsmoviefive', methods=['GET'])
+def route_get_apparaition_movie_five():
+    return get_apparaition_movie_five()
+
+@main.route('/apparitionsmoviesix', methods=['GET'])
+def route_get_apparaition_movie_six():
+    return get_apparaition_movie_six()
+
+@main.route('/apparitionsmovieseven', methods=['GET'])
+def route_get_apparaition_movie_seven():
+    return get_apparaition_movie_seven()
+
+
+@main.route('/apparitionsmovieeight', methods=['GET'])
+def route_get_apparaition_movie_eight():
+    return get_apparaition_movie_eight()
