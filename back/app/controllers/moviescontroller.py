967fac0c690cd0a7cd7a0dfccb764c3a3c62ea38
Juliette Mylle <154231973+JulietteMylle@users.noreply.github.com>
2025-04-17 10:05:50 +0200

    correction pourcentage

--- back/app/controllers/moviescontroller.py
+++ back/app/controllers/moviescontroller.py
@@ -6,9 +6,41 @@ from flask import jsonify
 
 def get_apparition_total():
     results = list(mongo.dialogs.aggregate([
-        {"$group" : {'_id' : "$character", 'apparitions' : {'$sum' : 1} }},
-        {"$project" : {"percentage" : {"$divide" : ["$apparitions", 100]}}},
-        {"$sort" : {"percentage" : -1}}
+         {
+        "$facet": {
+            "apparitions": [
+                {
+                    "$group": {
+                        "_id": "$character",
+                        "apparitions": {"$sum": 1}
+                    }
+                }
+            ],
+            "total": [
+                {
+                    "$group": {
+                        "_id": None,
+                        "total": {"$sum": 1}
+                    }
+                }
+            ]
+        }
+    },
+    {"$unwind": "$apparitions"},
+    {"$unwind": "$total"},
+    {
+        "$project": {
+            "character": "$apparitions._id",
+            "apparitions": "$apparitions.apparitions",
+            "percentage": {
+                "$multiply": [
+                    {"$divide": ["$apparitions.apparitions", "$total.total"]},
+                    100
+                ]
+            }
+        }
+    },
+    {"$sort": {"apparitions": -1}}
     ]))
     if results:
           return jsonify(results), 200
